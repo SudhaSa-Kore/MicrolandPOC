efeb3b446262b813fe53dcf149a9d34a51471b1b
Kstream 001 <kstream001@gmail.com>
2016-08-17 14:30:01 +0530

    images added

--- app.js
+++ app.js
@@ -654,21 +654,21 @@ app.post('/selctorTool',function(req,res){
 		if(req.body.payOffCategory === 'allOfIt'){
 			res.end(JSON.stringify({
                 "recommendedCard": "NAB Low Fee Card",
-                "url": "http://www.nab.com.au/personal/banking/credit-cards/checklist-nab-low-fee-card"
+                "url": "https://microlandpoc.herokuapp.com/images/ccsel-LowFee.jpg"
             }));
 		}else{
-          res.end(JSON.stringify({"recommendedCard":"NAB Low Rate Card","url":"http://www.nab.com.au/personal/banking/credit-cards/checklist-nab-low-rate-card"}));
+          res.end(JSON.stringify({"recommendedCard":"NAB Low Rate Card","url":"https://microlandpoc.herokuapp.com/images/ccsel-LowRate.jpg"}));
       }
     }else if(req.body.cardCategory === 'servicesCard'){
-      res.end(JSON.stringify({"recommendedCard":"NAB Premium Card","url":"http://www.nab.com.au/personal/banking/credit-cards/checklist-nab-premium-card"}));
+      res.end(JSON.stringify({"recommendedCard":"NAB Premium Card","url":"https://microlandpoc.herokuapp.com/images/ccsel-Premium.jpg"}));
     }else if(req.body.cardCategory === 'rewardsCard'){
         console.log('in rewards first if');
       if(req.body.rewardsProgram !== 'undefined' || req.body.rewardsProgram === 'qantasRewards'){
         console.log('in qantas card');
           if((req.body.monthlySpend === 'under1000' && req.body.payOffCategory === 'allOfIt') || (req.body.monthlySpend === 'under3000' && req.body.payOffCategory === 'allOfIt') || ((req.body.monthlySpend === 'under7000' && req.body.payOffCategory === 'allOfIt')|| (req.body.monthlySpend === 'under7000' && req.body.payOffCategory === 'itDepends')) || req.body.monthlySpend === 'over7000'){
-             res.end(JSON.stringify({"recommendedCard":"NAB Qantas Rewards Premium Card","url":"http://www.nab.com.au/personal/banking/credit-cards/checklist-nab-qantas-rewards-premium-card"}));
+             res.end(JSON.stringify({"recommendedCard":"NAB Qantas Rewards Premium Card","url":"https://microlandpoc.herokuapp.com/images/ccsel-QantasPremium.jpg"}));
          }else{
-             res.end(JSON.stringify({"recommendedCard":"NAB Qantas Rewards Card","url":"http://www.nab.com.au/personal/banking/credit-cards/checklist-nab-qantas-rewards-card"}));
+             res.end(JSON.stringify({"recommendedCard":"NAB Qantas Rewards Card","url":"https://microlandpoc.herokuapp.com/images/ccsel-Qantas.jpg"}));
          }
      }
     }else if(req.body.cardCategory === 'rewardsCard'){
@@ -676,14 +676,14 @@ app.post('/selctorTool',function(req,res){
     if(req.body.rewardsProgram !== 'undefined' || req.body.rewardsProgram === 'velocityRewards'){
         console.log('in velocity card');
       if((req.body.monthlySpend === 'under1000' && req.body.payOffCategory === 'allOfIt') || (req.body.monthlySpend === 'under3000' && req.body.payOffCategory === 'allOfIt') || ((req.body.monthlySpend === 'under7000' && req.body.payOffCategory === 'allOfIt')|| (req.body.monthlySpend === 'under7000' && req.body.payOffCategory === 'itDepends')) || req.body.monthlySpend === 'over7000'){
-         res.end(JSON.stringify({"recommendedCard":"NAB Velocity Rewards Premium Card","url":"http://www.nab.com.au/personal/banking/credit-cards/checklist-nab-velocity-rewards-premium-card"}));
+         res.end(JSON.stringify({"recommendedCard":"NAB Velocity Rewards Premium Card","url":"https://microlandpoc.herokuapp.com/images/ccsel-VelocityPremium.jpg"}));
      }else{
-         res.end(JSON.stringify({"recommendedCard":"NAB Velocity Rewards Card","url":"http://www.nab.com.au/personal/banking/credit-cards/checklist-nab-velocity-rewards-card"}));
+         res.end(JSON.stringify({"recommendedCard":"NAB Velocity Rewards Card","url":"https://microlandpoc.herokuapp.com/images/ccsel-Velocity.jpg"}));
      }
  }
 }else if(req.body.cardCategory === 'rewardsCard'){
   if(req.body.rewardsProgram !== 'undefined' && req.body.rewardsProgram === 'flybuysRewards'){
-      res.end(JSON.stringify({"recommendedCard":"NAB flybuys Rewards Card","url":"http://www.nab.com.au/personal/banking/credit-cards/checklist-nab-flybuys-rewards-card"}));
+      res.end(JSON.stringify({"recommendedCard":"NAB flybuys Rewards Card","url":"https://microlandpoc.herokuapp.com/images/ccsel-flybuys.jpg"}));
   }
 }  
 
